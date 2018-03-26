b0707f35e8d15c3ca340bfb5edcb9531aee9c369
rqguzman <rafael.guzman@outlook.com>
2018-03-26 14:08:28 -0300

    Normalized server port

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000; //ARBITRANDO UMA PORTA
+//ARBITRANDO UMA PORTA -> migrou para a Fç normalizeport()
+// const port = 3000;
+// RECEBENDO A PORTA DO SERVIDOR
+/** A porta já deve vir informada pelo servidor (Azure, Heroku Etc).
+ * O que está sendo passado como parâmetro para a Fç normalizePort()
+ * é a porta que consta em environment(env)
+ * Caso não venha, arbitrams a porta 3000
+ */
+const port = normalizePort(process.env.PORT || 3000);
 app.set('port', port); // SETANDO A PORTA P/ APLICAÇÃO
 
 //CRIANDO UM SERVIDOR
@@ -33,4 +41,22 @@ app.use('/', route);
 
 server.listen(port);
 console.log('API rodando na porta 3000...');
+
+// Função retirada do gerador de código do Express
+function normalizePort(valor) {
+    const port = parseInt(valor, 10);
+
+    // Se não for um número, retorna um número(10, no caso)
+    if (isNaN(port)) {
+        return val;
+    }
+
+    // Se for maior ou igual a zero, retorna a porta
+    if (port >=0 ) {
+        return port;
+    }
+
+    // Caso contrário, não retornamos nada()
+    return false;
+}
 
