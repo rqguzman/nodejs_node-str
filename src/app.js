23ea370cce54499cdfc20880668ef542020bab4f
rqguzman <rafael.guzman@outlook.com>
2018-03-30 10:20:37 -0300

    Organized API routes in proper directory

--- src/app.js
+++ src/app.js
@@ -6,41 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router(); // ARQUIVO DE ROTAS
 
+// Carregando as rotas
+const indexRoutes = require('./routes/index-routes');
+const productRoutes = require('./routes/product-routes');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-//ROTAS
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
 
 // URLs
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoutes);
+app.use('/products', productRoutes);
 
 module.exports = app;
