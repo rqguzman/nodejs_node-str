e65049d5c75de4cada909fe6900115320aa30b39
rqguzman <rafael.guzman@outlook.com>
2018-04-02 10:03:45 -0300

    Installed mongoose dependency

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router(); // ARQUIVO DE ROTAS
+
+// Conectando ao DB
+const strgConnection = 'mongodb://rqguzman:10312556@Hq@ds231589.mlab.com:31589/ndstr_guz';
+mongoose.connect(strgConnection);
 
 // Carregando as rotas
 const indexRoutes = require('./routes/index-routes');
