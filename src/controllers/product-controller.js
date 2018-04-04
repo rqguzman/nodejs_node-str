b66568e31815d783279b85f8947fd2030cdb59b9
rqguzman <rafael.guzman@outlook.com>
2018-04-04 14:06:36 -0300

    Implemented http get methods

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,11 +3,70 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product.find({
+        active: true
+    }, 'title price slug').then(data => {
+        res
+            .status(200)
+            .send(data);
+    }).catch(error => {
+        res
+            .status(400)
+            .send(error);
+    });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({
+        slug: req.params.slug,
+        active: true
+    }, 'title price slug tags').then(data => {
+        res
+            .status(200)
+            .send(data);
+    }).catch(error => {
+        res
+            .status(400)
+            .send(error);
+    });
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById({id: req.params.id})
+        .then(data => {
+            res
+                .status(200)
+                .send(data);
+        })
+        .catch(error => {
+            res
+                .status(400)
+                .send(error);
+        });
+}
+
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tags: req.params.tag,
+        active: true
+    }, 'title description price slug tags').then(data => {
+        res
+            .status(200)
+            .send(data);
+    }).catch(error => {
+        res
+            .status(400)
+            .send(error);
+    });
+}
+
 exports.post = ('/', (req, res, next) => {
     var product = new Product(req.body);
     // product.title = req.body.title; 
     // product.slug = req.body.slug;
-    // product.description = req.body.description;
+    // product.description = req.body.description; 
     // product.price = req.body.price;
     // product.active = req.body.active;
     // product.tags = req.body.tags;
--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -5,6 +5,10 @@ const router = express.Router(); // ARQUIVO DE ROTAS
 const controller = require('../controllers/product-controller');
 
 //ROTAS
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
