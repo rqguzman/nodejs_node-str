94fa23cd0c272a5afc7fbeb0e17ff921e0abdbca
rqguzman <rafael.guzman@outlook.com>
2018-04-04 08:54:35 -0300

    Created a model

---
+++ src/models/product.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    // _id criado automaticamente
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: { // Nome Produto = nome-produto
+        type: String,
+        required: [true, 'o slug é obrigatório'],
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags: [
+        {
+            type: String,
+            required: true
+        }
+    ]
+});
+
+module.exports = mongoose.model('Product', schema);
