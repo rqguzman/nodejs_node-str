c0fb2d8f79e5151de750520a822fe5fb213b2c3f
rqguzman <rafael.guzman@outlook.com>
2018-04-05 19:56:42 -0300

    Implemented a repository

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,39 +3,41 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/product-repository');
 
 exports.get = (req, res, next) => {
-    Product.find({
-        active: true
-    }, 'title price slug').then(data => {
-        res
-            .status(200)
-            .send(data);
-    }).catch(error => {
-        res
-            .status(400)
-            .send(error);
-    });
+    repository
+        .get()
+        .then(data => {
+            res
+                .status(200)
+                .send(data);
+        })
+        .catch(error => {
+            res
+                .status(400)
+                .send(error);
+        });
 }
 
 exports.getBySlug = (req, res, next) => {
-    Product.findOne({
-        slug: req.params.slug,
-        active: true
-    }, 'title price slug tags').then(data => {
-        res
-            .status(200)
-            .send(data);
-    }).catch(error => {
-        res
-            .status(400)
-            .send(error);
-    });
+    repository
+        .getBySlug(req.params.slug)
+        .then(data => {
+            res
+                .status(200)
+                .send(data);
+        })
+        .catch(error => {
+            res
+                .status(400)
+                .send(error);
+        });
 }
 
 exports.getById = (req, res, next) => {
-    Product
-        .findById({id: req.params.id})
+    repository
+        .getById(req.params.id)
         .then(data => {
             res
                 .status(200)
@@ -49,18 +51,18 @@ exports.getById = (req, res, next) => {
 }
 
 exports.getByTag = (req, res, next) => {
-    Product.find({
-        tags: req.params.tag,
-        active: true
-    }, 'title description price slug tags').then(data => {
-        res
-            .status(200)
-            .send(data);
-    }).catch(error => {
-        res
-            .status(400)
-            .send(error);
-    });
+    repository
+        .getByTag(req.params.tag)
+        .then(data => {
+            res
+                .status(200)
+                .send(data);
+        })
+        .catch(error => {
+            res
+                .status(400)
+                .send(error);
+        });
 }
 
 exports.post = (req, res, next) => {
@@ -73,14 +75,15 @@ exports.post = (req, res, next) => {
 
     // Se dados forem inválidos
     if (!contract.isValid()) {
-        res.status(400).send(contract.errors()).end();
+        res
+            .status(400)
+            .send(contract.errors())
+            .end();
         return;
     }
 
-    var product = new Product(req.body);
-    
-    product
-        .save()
+    repository
+        .create(req.body)
         .then(x => {
             res
                 .status(201)
@@ -94,15 +97,8 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    Product
-        .findByIdAndUpdate(req.params.id, {
-        $set: {
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price,
-            slug: req.body.slug
-        }
-    })
+    repository
+        .update(req.params.id, req.body)
         .then(x => {
             res
                 .status(200)
@@ -116,8 +112,8 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-    Product
-        .findOneAndRemove(req.body.id)
+    repository
+        .delete(req.body.id)
         .then(x => {
             res
                 .status(200)
