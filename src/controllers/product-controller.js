32267851eea5d6dc862b4dd82733e5c1da25094d
rqguzman <rafael.guzman@outlook.com>
2018-04-05 09:22:21 -0300

    Implemented a delete method

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -103,8 +103,17 @@ exports.put = (req, res, next) => {
         });
 };
 
-exports.delete = ('/', (req, res, next) => {
-    res
-        .status(200)
-        .send(req.body);
-});
+exports.delete = (req, res, next) => {
+    Product
+        .findOneAndRemove(req.body.id)
+        .then(x => {
+            res
+                .status(200)
+                .send({message: 'Produto removido com sucesso!'});
+        })
+        .catch(error => {
+            res
+                .status(400)
+                .send({message: 'Falha ao remover o produto.', data: error});
+        });
+};
