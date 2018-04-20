0b4720f7eedb39fb655fe45aeef91aeeb35262c9
rqguzman <rafael.guzman@outlook.com>
2018-04-20 13:26:35 -0300

    implemented a refresh token logic. removed customer data from Req body

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -4,6 +4,7 @@
 // const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/order-repository');
 const guid = require('guid');
+const authService = require('../services/auth-service');
 
 exports.get = async(req, res, next) => {
     try {
@@ -36,9 +37,14 @@ exports.post = async(req, res, next) => {
     // }
 
     try {
+        // Recupera o token
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];  
+        // Decodifica o token
+        const data = await authService.decodeToken(token);
+
         await repository.create({
-            customer:  req.body.customer,
-            number: guid.raw().substring(0,6),
+            customer:  data.id,
+            number: guid.raw().substring(0, 6),
             items: req.body.items
         });
         res
