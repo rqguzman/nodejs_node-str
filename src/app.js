f9c1871b6b9cd99fbef32d0110927363259704d5
rqguzman <rafael.guzman@outlook.com>
2018-04-24 14:05:15 -0300

    Enabled bodyParser and CORS settings

--- src/app.js
+++ src/app.js
@@ -22,9 +22,18 @@ const productRoutes = require('./routes/product-routes');
 const customerRoutes = require('./routes/customer-routes');
 const orderRoutes = require('./routes/order-routes');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false}));
 
+//Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Origin', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 // URLs
 app.use('/', indexRoutes);
